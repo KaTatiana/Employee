717504bab86312068ef7f1fa599cbd6ae3f4e370
KaTatiana <badinat@mail.ru>
2022-09-22 00:18:50 +0300

    main

---
+++ src/main/java/proskyemployee/Employee.java
@@ -0,0 +1,13 @@
+package proskyemployee;
+public class Employee {
+    private final String firstName;
+    private final String lastName;
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName=lastName;
+    }
+
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+}
